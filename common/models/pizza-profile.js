1dfae6c754254a37b371b2de187a000a1f4c7e7c
Karan Shah <Karan Shah>
2017-03-23 19:49:16 -0400

    RESTful API Service

---
+++ common/models/pizza-profile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = function(Pizzaprofile) {
+  Pizzaprofile.status = function(cb) {
+    var currentDate = new Date();
+    var currentHour = currentDate.getHours();
+    var OPEN_HOUR = 6;
+    var CLOSE_HOUR = 15;
+    console.log('Current hour is %d', currentHour);
+    var response;
+    if (currentHour > OPEN_HOUR && currentHour < CLOSE_HOUR) {
+      response = 'We are open for business.';
+    } else {
+      response = 'Sorry, we are closed. Open daily from 6am to 3pm.';
+    }
+    cb(null, response);
+  };
+  Pizzaprofile.remoteMethod(
+    'status', {
+      http: {
+        path: '/status',
+        verb: 'get'
+      },
+      returns: {
+        arg: 'status',
+        type: 'string'
+      }
+    }
+  );
+
+};
